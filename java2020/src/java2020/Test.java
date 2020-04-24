435e5340cb209f5c15c3c6ae56e7a53918b2710d
ymy12x <ymy12@hanmail.net>
2020-04-24 23:58:46 +0900

    method추가 및 호출

--- java2020/src/java2020/Test.java
+++ java2020/src/java2020/Test.java
@@ -1,9 +1,15 @@
 package java2020;
 
 public class Test {
+	
+	static void method() {
+		System.out.println("abc");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		
+		method();
 
 	}
 
